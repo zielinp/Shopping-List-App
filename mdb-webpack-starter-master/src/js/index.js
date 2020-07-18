582d6a304aa2f71cec1054a8ad406b598d3d0301
Paulina Zielinska <pzielinska.zielona@gmail.com>
2020-07-18 22:41:05 +0200

    Adding remove and check button for single and all products

--- mdb-webpack-starter-master/src/js/index.js
+++ mdb-webpack-starter-master/src/js/index.js
@@ -4,7 +4,7 @@ export default {
   mdb,
 };
 
-//const main_product_list =[];
+var main_product_list = [];
 
 
 class Product {
@@ -16,8 +16,47 @@ class Product {
   }
 }
 
+// Function to sort products by category
+function compare(a, b) {
+  // Use toUpperCase() to ignore character casing
+  const categoryA = a.category.toUpperCase();
+  const categoryB = b.category.toUpperCase();
+
+  let comparison = 0;
+  if (categoryA > categoryB) {
+    comparison = 1;
+  } else if (categoryA < categoryB) {
+    comparison = -1;
+  }
+  return comparison;
+}
 
 
+
+// Delete product
+function deleteProduct(){
+  const item_to_delete = this.closest(".list-product");
+  item_to_delete.remove();
+}
+
+// Delete all products
+function deleteAllProducts(){
+  $(".list-product").remove();
+}
+
+// Check all products
+function checkAllProducts(){
+   $(".form-check-input").prop('checked', $("#checkAll").prop('checked'));
+}
+
+//Uncheck product
+function uncheckProduct(){
+  $("#checkAll").prop('checked', false);
+}
+
+
+document.getElementById("deleteAll").addEventListener("click",deleteAllProducts);
+document.getElementById("checkAll").addEventListener("click",checkAllProducts);
 
 // Create a new list item
 function createListItem() {
@@ -30,7 +69,19 @@ function createListItem() {
 
   const product_group = document.createElement("ul");
   product_group.classList.add("list-group", "list-group-horizontal", "row");
+  // product_group.contentEditable = "true";
   list_product.appendChild(product_group);
+
+  const checked_product = document.createElement("li");
+  checked_product.classList.add("list-group-item", "product-item", "col-1");
+  product_group.appendChild(checked_product);
+
+  const checked_product_input = document.createElement("input");
+  checked_product_input.classList.add("form-check-input");
+  checked_product_input.type = "checkbox";
+  checked_product_input.value ="";
+  checked_product.appendChild(checked_product_input);
+  checked_product_input.addEventListener("click",uncheckProduct);
 
   const item = document.createElement("li");
   item.classList.add("list-group-item", "product-item", "col-3");
@@ -38,12 +89,12 @@ function createListItem() {
   item.innerText = new_product.name;
 
   const qunatity = document.createElement("li");
-  qunatity.classList.add("list-group-item", "quantity-item", "col-3");
+  qunatity.classList.add("list-group-item", "quantity-item", "col-2");
   product_group.appendChild(qunatity);
   qunatity.innerText = new_product.qunatity;
 
   const unit = document.createElement("li");
-  unit.classList.add("list-group-item", "unit-item", "col-3");
+  unit.classList.add("list-group-item", "unit-item", "col-2");
   product_group.appendChild(unit);
   unit.innerText = new_product.unit;
 
@@ -52,6 +103,15 @@ function createListItem() {
   product_group.appendChild(category);
   category.innerText = new_product.category;
 
+  const delete_item = document.createElement("li");
+  delete_item.classList.add("list-group-item", "product-item", "col-1");
+  product_group.appendChild(delete_item);
+  
+  const delete_item_button = document.createElement("button");
+  delete_item_button.classList.add("btn", "btn-danger", "btn-sm", "btn-floating");
+  delete_item.appendChild(delete_item_button);
+  delete_item_button.innerHTML = "<i class='fas fa-times'></i>";
+  delete_item_button.addEventListener("click", deleteProduct);
 }
 
 // Take values from input
@@ -69,6 +129,9 @@ function takeValuesFromInput() {
   document.getElementById("productCategory").value = "";
   document.getElementById("addButton").disabled = true;
 
+  main_product_list.push(new_product);
+  console.log(main_product_list);
+  console.log(main_product_list.sort(compare));
   return new_product;
 }
 
@@ -89,10 +152,4 @@ function validateForm() {
 document.getElementById("productName").addEventListener("change", validateForm);
 document.getElementById("productQuantity").addEventListener("change", validateForm);
 document.getElementById("productCategory").addEventListener("change", validateForm);
-
-
-
-
-
-
 document.getElementById("addButton").addEventListener("click", createListItem)
